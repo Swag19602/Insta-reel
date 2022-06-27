7ddcf42bc8d8dab8a631008ac46ba96fa731d159
Swag19602 <swagatambhattacharjee02@gmail.com>
2022-06-28 00:45:28 +0530

    tiktok-swag

---
+++ data.js
@@ -0,0 +1,31 @@
+// import mahebooba from '../tiktok/src/video/mahebooba.mp4'
+const data=[
+    {url:'file:///D:/My%20Placement%20Preperation/Data%20Structures/Web%20Development/MyProjects/Tik%20Tok/tiktok/src/video/mahebooba.mp4' ,
+    channel:'kgf' ,
+    description:'KGF 2 romantic song #trending' ,
+    song:'Mahebooba' ,
+    likes:"124" ,
+    messages:"345",
+     shares:"5432",
+    },
+   {  url:'file:///D:/My%20Placement%20Preperation/Data%20Structures/Web%20Development/MyProjects/Tik%20Tok/tiktok/src/video/kiara.mp4',
+   channel:'kiara',
+    description:'kiara and sushant',
+     song:'Khuda jane',
+     likes:"13",
+      messages:"35" ,
+      shares:"542", 
+    },
+   { 
+     url:'file:///D:/My%20Placement%20Preperation/Data%20Structures/Web%20Development/MyProjects/Tik%20Tok/tiktok/src/video/WhatsApp%20Video%202022-04-28%20at%201.10.55%20AM.mp4' ,
+   channel:'kgf',
+    description:'KGF 2 SULTAN SONG #trending' ,
+    song:'DHEERA DHEERA',
+    likes:"1234" ,
+    messages:"345" ,
+    shares:"432"
+}
+
+
+]
+export default data;
---
+++ server.js
@@ -0,0 +1,54 @@
+import express, { response } from 'express';
+import mongoose from 'mongoose';
+import myVideos from './dbModel.js';
+import data from './data.js';
+const app=express();
+const port=process.env.port || 9000;
+//middle wares
+app.use(express.json());
+app.use((req,res,next)=>{
+    res.setHeader('Acess-Control-Allow-Origin','*'),
+    res.setHeader('Acess-Control-Allow-Headers','*'),
+    next()
+})
+//db config
+const connection_url='mongodb+srv://Swag19602:Swagbidisha1986@cluster0.ulrjfde.mongodb.net/tiktok?retryWrites=true&w=majority'
+mongoose.connect(connection_url,{
+    useNewUrlParser: true,
+//   useCreateIndex:false,
+  useUnifiedTopology: true,
+})
+
+//api endpoints
+app.get('/',(req,res)=>{
+    res.status(200).send('Hello world')
+})
+app.get('/v1/posts',(req,res)=>{
+    res.status(200).send(data);
+})
+app.post('/v2/posts',(req,res)=>{
+    const dbVideos=req.body;
+    myVideos.create(dbVideos,(err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(201).send(data);
+        }
+    })
+  
+})
+app.get('/v2/posts',(req,res)=>{
+    myVideos.find({},(err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send(data);
+        }
+    })
+})
+//api listen points
+app.listen(port, (req,res)=>{
+    console.log(`server is running at localhost: ${port}`);
+})
